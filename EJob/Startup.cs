c83abe31f152bd5286c028a46bdb5bac5e72b4ac
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2019-03-27 22:16:30 +0100

    Setting up static files to run the angular frontend

--- EJob/Startup.cs
+++ EJob/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -41,7 +42,18 @@ namespace EJob
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+            app.Use(async (context,next) => {
+                await next();
+                if(context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+
+            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
