591fe142a28d3dcc937fb5ffa6fedfadb7774c27
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2019-04-01 19:22:26 +0100

    Setting up EntityFrameworkCore with MySql database

--- EJob/Startup.cs
+++ EJob/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using EJob.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,6 +29,9 @@ namespace EJob
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<EjobContext>(
+                options=> options.UseMySql(Configuration.GetConnectionString("Default"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
