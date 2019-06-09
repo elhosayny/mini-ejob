691ebfd8321111a02ed520a641fff3539d5e2d2a
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2019-06-09 22:17:22 +0100

    Refactor UserController

---
+++ EJob/Contracts/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using EJob.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace EJob.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> CreateAsync(UserModel userModel);
+    }
+}
---
+++ EJob/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using EJob.Contracts;
+using EJob.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EJob.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public UserRepository(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IdentityResult> CreateAsync(UserModel userModel)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = userModel.Username,
+                Email = userModel.Email,
+                FirstName = userModel.FirstName,
+                LastName = userModel.LastName
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, userModel.Password);
+            return result;
+        }
+    }
+}
